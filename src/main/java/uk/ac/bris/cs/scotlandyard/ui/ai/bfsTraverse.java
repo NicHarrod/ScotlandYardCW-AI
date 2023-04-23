282701297200c5949becfc59429001e173e8f5b1
AHMED TOUGHAN <AhmedToughan19@gmail.com>
2023-04-24 00:44:37 +0100

    written bfs

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/bfsTraverse.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/bfsTraverse.java
@@ -1,21 +1,71 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
+import java.util.*;
 
+import com.esotericsoftware.kryo.util.Null;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.graph.ImmutableValueGraph;
+import javafx.css.Size;
+import uk.ac.bris.cs.scotlandyard.model.Board;
+import uk.ac.bris.cs.scotlandyard.model.Move;
+import uk.ac.bris.cs.scotlandyard.model.Piece;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard;
+
 
 import javax.annotation.Nonnull;
 
 public class bfsTraverse {
     public ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph;
+    public ArrayList<Integer> path;
 
-    public bfsTraverse(@Nonnull ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph){
+    public bfsTraverse(@Nonnull ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph, Integer start, Integer end){
+        HashMap<Integer, Integer> prev = solve(start);
+        this.path = PathFromEndToStart(start, end, prev);
         this.graph=graph;
-
-
     }
 
-    public int findNextDetective (int Source){
-        return 0;
+
+
+    public HashMap<Integer, Integer> solve (@Nonnull Integer start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        HashMap<Integer, Boolean> visited = new HashMap<>(graph.nodes().size());
+        visited.put(start, false);
+        HashMap<Integer, Integer> prev = new HashMap<>(graph.nodes().size());
+        for (Integer node : graph.nodes()) {
+            prev.put(node, -1);
+            visited.put(node, false);
+        }
+        visited.put(start, true);
+        ArrayList<Integer> neighbours;
+        while (!queue.isEmpty()) {
+            Integer parent = queue.remove();
+            neighbours = (ArrayList<Integer>) this.graph.adjacentNodes(parent);
+            for (Integer next : neighbours) {
+                if (visited.get(next) == false) {
+                    queue.add(next);
+                    visited.put(next, true);
+                    prev.put(next, parent);
+                }
+            }
+        }
+        return prev;
+    }
+
+    public ArrayList<Integer> PathFromEndToStart(Integer start,Integer end,HashMap<Integer, Integer> prev){
+        ArrayList<Integer> path = new ArrayList<>();
+        for (Integer at = end; at != null; at = prev.get(at)) {
+            path.add(at);
+        }
+        Collections.reverse(path);
+        if (path.get(0) == start) {
+            return path;
+        }
+        return null;
+    }
+
+    public HashMap<Piece, Integer> findNextDetective (int Source){
+        HashMap<Piece, Integer> detectives = new HashMap<>();
+        Queue<Integer> queue = new LinkedList<>();
+        return detectives;
     }
 }
